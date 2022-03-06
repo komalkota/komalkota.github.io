ddbf97eaabfd49b342f9859e4f8984d7ef056ec9
komalkota <78479892+komalkota@users.noreply.github.com>
2022-03-06 16:34:13 +0530

    Add files via upload

---
+++ feb 28/Fizzbuzz.js
@@ -0,0 +1,14 @@
+
+for (var i=1; i <= 20; i++)
+{
+    if (i % 15 == 0)
+        console.log("FooBar");
+    else if (i % 3 == 0)
+        console.log("Foo");
+    else if (i % 5 == 0)
+        console.log("Bar");
+    else
+        console.log(i);
+}
+
+
